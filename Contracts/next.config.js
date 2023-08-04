8691d2d8cdd194cc069e09d8f6f458c12d07fb82
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-08-03 14:47:30 -1000

    Update next.config.js

--- Contracts/next.config.js
+++ Contracts/next.config.js
@@ -1,6 +1,4 @@
-/** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
